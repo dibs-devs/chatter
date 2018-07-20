4694880f12cfa62be43a1eb7542142a15fa0559a
Ahmed Ishtiaque <ahmedishti27@gmail.com>
2018-07-20 10:53:58 -0400

    Minor bugfix

--- chat/models.py
+++ chat/models.py
@@ -2,5 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class UserMessage(models.Model):
-	message = models.ForeignKey(User, on_delete = models.PROTECT)
+#This model is used to give date and time when a message was created/modified.
+class MessagesDateModel(models.Model):
+	date_created = models.DateTimeField(auto_now_add = True)
+	date_modified = models.DateTimeField(auto_now = True)
+	time_modified = models.TimeField(auto_now_add = True)
+
+	class Meta:
+		abstract = True
+
