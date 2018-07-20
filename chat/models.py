59943005ae4c2f3541aa89cd1e5a5a4ceda9d70b
dibs.devs <dibs.devs@gmail.com>
2018-07-20 12:29:00 -0400

    Added a room model

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-#This model is used to give date and time when a message was created/modified.
+
+# This model is used to give date and time when a message was created/modified.
 class DateTimeModel(models.Model):
-	date_created = models.DateTimeField(auto_now_add = True)
-	date_modified = models.DateTimeField(auto_now = True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
 
-# class Message()
+
+class Room(DateTimeModel):
+    title = models.CharField(max_length=20)
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    users = models.ManyToManyField(User, on_delete=models.PROTECT)
+
+    @property
+    def is_private(self):
+        return self.users.objects.count() == 1
+
+
