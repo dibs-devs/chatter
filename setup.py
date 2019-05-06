0c194ce2a8005b68233517584e11afe4a50e730a
Dibs <39922867+dibs-devs@users.noreply.github.com>
2019-05-05 22:24:39 -0400

    Update setup.py to reflect release 0.1.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-chatter',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
@@ -33,7 +33,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'shortuuid==0.5.0',
         'channels==2.1.7',
         'bleach==3.1.0',
         'django>=2.0.9, <3',
